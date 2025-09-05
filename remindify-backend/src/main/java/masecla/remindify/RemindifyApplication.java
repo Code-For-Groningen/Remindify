5634f19a7a9c5abb72da010c797ab82ebc4c503d
masecla22 <supermasecla22@gmail.com>
2025-09-05 13:21:00 +0200

    Added boilerplate for backend

---
+++ remindify-backend/src/main/java/masecla/remindify/RemindifyApplication.java
@@ -0,0 +1,13 @@
+package masecla.remindify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemindifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RemindifyApplication.class, args);
+	}
+
+}
---
+++ remindify-backend/src/test/java/masecla/remindify/RemindifyApplicationTests.java
@@ -0,0 +1,13 @@
+package masecla.remindify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RemindifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
